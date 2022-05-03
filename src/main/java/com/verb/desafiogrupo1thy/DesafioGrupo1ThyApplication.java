90883403a22680b0346f61196b48294eff935a30
amaral.carolina <w.anapereira@gmail.com>
2022-05-03 13:17:06 -0300

    Firts commit

---
+++ src/main/java/com/verb/desafiogrupo1thy/DesafioGrupo1ThyApplication.java
@@ -0,0 +1,13 @@
+package com.verb.desafiogrupo1thy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGrupo1ThyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGrupo1ThyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verb/desafiogrupo1thy/DesafioGrupo1ThyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verb.desafiogrupo1thy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGrupo1ThyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
